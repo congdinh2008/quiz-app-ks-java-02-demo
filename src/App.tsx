014121072492ef5bfda46e1c48e47a8151e16beb
Cong Dinh <congdinh2012@hotmail.com>
2025-09-12 16:06:54 +0700

    feat: add Font Awesome icons to the quiz app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
 
   return (
     <>
       <h1 className="text-2xl font-bold underline">
-        Hello world!
+        Hello world! 
+        <FontAwesomeIcon icon={faGlobe} className="text-blue-500 ml-2"/>
       </h1>
     </>
   )
